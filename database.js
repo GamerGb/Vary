6c91e55fe8ff8c5f95ef00c54984e2516a6db24b
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-04-14 15:59:44 +0100

    :-:

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Discord = require('discord.js')
 
-mongoose.connect('process.env.DB', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.DB, { useNewUrlParser: true }, (err) => {
   if (err) return console.log('Erro ao conectar na database!')
-  console.log('Conectado ao banco de dados!')
+  console.log('Conectado ao BANCO DE DADOS!')
 })
 
 const Users = new Schema({
